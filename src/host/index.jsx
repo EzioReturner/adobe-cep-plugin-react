6d30389ec623f97c94e431ce920e1db35f61dfc3
ezio <ezioreturner@gmail.com>
2021-11-29 15:17:57 +0800

    调整host 与lib

--- src/host/index.jsx
+++ src/host/index.jsx
@@ -1,5 +1,18 @@
 if (typeof $ == 'undefined') $ = {};
 
+try {
+  var xLib = new ExternalObject('lib:PlugPlugExternalObject');
+} catch (e) {
+  // do nothing
+}
+
+function dispatch(message) {
+  var eventObj = new CSXSEvent();
+  eventObj.type = 'lucky_event_log';
+  eventObj.data = message;
+  eventObj.dispatch();
+}
+
 $._ext = {
   //Evaluate a file and catch the exception.
   evalFile: function (path) {
@@ -25,4 +38,57 @@ $._ext = {
 var creatNewDocument = function (params) {
   //使用photoshop api创建文档
   app.documents.add(100, 100, 72, 'test');
+
+  return 'add complated';
+};
+
+var getDocuments = function () {
+  var names = [];
+
+  for (var i = 0; i < app.documents.length; i++) {
+    names.push(app.documents[i].name);
+  }
+
+  return names || [];
+};
+
+var getActiveDocument = function () {
+  return app.activeDocument.name;
+};
+
+var getLayers = function () {
+  var layers = app.activeDocument.layers;
+
+  var names = [];
+
+  for (var i = 0; i < layers.length; i++) {
+    names.push(layers[i].name);
+  }
+
+  return names;
+};
+
+var setActiveDocument = function (name) {
+  var doc = app.documents.getByName(name);
+  app.activeDocument = doc;
+
+  return 'success';
+};
+
+var stringIDToTypeID = function (stringId) {
+  var TypeId = app.stringIDToTypeID(stringId);
+
+  return TypeId;
+};
+
+var testSavePng = function () {
+  var pngfile = new File('/Users/zhev/Downloads/temp001.png');
+
+  var pngSaveOptions = new PNGSaveOptions();
+
+  pngSaveOptions.compression = 1;
+
+  app.activeDocument.saveAs(pngfile, pngSaveOptions, true, Extension.LOWERCASE);
+
+  return 'save success';
 };
